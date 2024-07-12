f71c6e1bcdbe0f352dadae4c15e5174723453b41
Rathore-Prakhar <123528354+Rathore-Prakhar@users.noreply.github.com>
2024-07-11 22:00:54 -0700

    more functions

--- malloc.c
+++ malloc.c
@@ -61,3 +61,52 @@ void* realloc(void* block, size_t size) {
 
     return ret;
 }
+
+void free(void* block) {
+    header_t* header, *tmp;
+    void* programbreak;
+    if (!block) {
+        return;
+    }
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (header_t*)block - 1;
+
+    programbreak = sbrk(0);
+
+    if ((char*)block + header->size == programbreak) {
+        if (head == tail) {
+            head = tail = NULL;
+        } else {
+            tmp = head;
+            while (tmp->next != tail) {
+                tmp = tmp->next;
+            }
+            tmp->next = NULL;
+            tail = tmp;
+        }
+        sbrk(0 - sizeof(header_t) - header->size);
+    } else {
+        header->is_free = 1;
+    }
+
+    pthread_mutex_unlock(&global_malloc_lock);
+}
+
+void* calloc(size_t num, size_t nsize) {
+    size_t size;
+    void* block;
+    if (!num || !nsize) {
+        return NULL;
+    }
+    size = num * nsize;
+    if (nsize != size / num) {
+        return NULL;
+    }
+    block = malloc(size);
+    if (!block) {
+        return NULL;
+    }
+    memset(block, 0, size);
+
+    return block;
+}
